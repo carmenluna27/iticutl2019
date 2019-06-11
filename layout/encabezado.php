5a7e37c6a0bc59bcf0bb3ffb876b83c1af72943c
Carmen2706 <carmenluna2706@gmail.com>
2019-06-10 23:49:50 -0500

    Login

--- layout/encabezado.php
+++ layout/encabezado.php
@@ -1,30 +1,31 @@
 		<section class="contenedor iconos fondo">
 			<ul class="nav-pills pull-right menu-bar">
-				<li class="list-unstyled">
+				<!-- <li class="list-unstyled">
 					<a href="#" class="color  borde" id="menuV">
 						<i class="fas fa-bars"></i>
 					</a>
-				</li>
+				</li> -->
 				<li class="list-unstyled">
 					<a href="#" class="color borde">
-						<i class="fas fa-question-circle"></i>
+					<i class="far fa-user-circle"></i>
 					</a>
 				</li>
 				<li class="list-unstyled">
 					<a href="#" class="color borde">
-						<i class="far fa-user-circle"></i>
+						<i class="fas fa-unlock-alt"></i>
 					</a>
 				</li>
 				<li class="list-unstyled">
 					<a href="#" onclick="salir();" class="color borde">
-						<i class="fas fa-power-off"></i>
+						<i class="fas fa-sign-out-alt"></i>
 					</a>
 				</li>
 
 				<li class="list-unstyled">
 					<p  class="user fondo">
-						Pablo Adrian Perez Briseño
+						Maria del Carmen Luna Cuellar
 					</p>
 				</li>
 			</ul>
 		</section>
+		
--- layout/menuv.php
+++ layout/menuv.php
@@ -1,15 +1,18 @@
 				<div class="sidebar fondo borde fuenteAzul sombra" >
 					<h2 class="fondo">Menú</h2>
 					<ul>
-					<li class="list-unstyled">
-							<a href="../inicio/index.php" class="activo"><i class="fas fa-home"></i> Inicio</a>
+						<li class="list-unstyled">
+						<a href="#" class="activo"><i class="fas fa-home"></i> Inicio</a>
 						</li>
 						<li class="list-unstyled">
 							<a href="../mPersonas/index.php"><i class="fas fa-user-tie"></i> Personas</a>
 						</li>
 						<li class="list-unstyled">
-							<a href="../mAlumnos/index.php" ><i class="fas fa-user-graduate"></i> Alumnos </a>
+							<a href="../mAlumnos/index.php"><i class="fas fa-user-graduate"></i> Alumnos </a>
 						</li >
+						<li class="list-unstyled">
+							<a href="../mFotografia/index.php"><i class="far fa-file-image"></i> Subir Fotografía </a>
+						</li>
 						<li class="list-unstyled">
 							<a href="../mCarreras/index.php"><i class="fas fa-running"></i> Carreras</a>
 						</li>
@@ -17,7 +20,7 @@
 							<a href="../mUsuarios/index.php"><i class="fas fa-user"></i> Usuarios</a>
 						</li>
 						<li class="list-unstyled">
-							<a href="#" onclick="salir();"><i class="fas fa-power-off"></i> Salir</a>
+						<a href="#" onclick="salir();"><i class="fas fa-sign-out-alt"></i> Salir</a>
 						</li>
 					</ul>
 				</div>
